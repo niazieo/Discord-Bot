6a084c02a15459378e99a68f326454c364509722
niazieo <omar.niazie@gmail.com>
2022-02-15 01:05:34 -0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -8,10 +8,10 @@ module.exports = {
     description: 'Clear messages.',
     async execute(message, args, cmd, client, Discord){
         if (!args[0]) return message.reply('Enter the amount of messages you want to delete');
-        if (isNaN(args[0])) return message.reply('Please enter a real number dumb dumb');
+        if (isNaN(args[0])) return message.reply('Please enter a real number');
 
         if (args[0] > 100) return message.reply('Cant do more than 100 messages, sorry!');
-        if (args[0] < 1) return message.reply('You must delete at least one message bro!');
+        if (args[0] < 1) return message.reply('You must delete at least one message!');
 
         await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
             message.channel.bulkDelete(messages);
