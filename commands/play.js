5b44930dc68745601acaeb1f0fd6b806a180a770
niazieo <omar.niazie@gmail.com>
2022-02-16 00:59:52 -0700

    Update play.js
    -Removed old music stuff
    -added try/catch

--- commands/play.js
+++ commands/play.js
@@ -98,62 +98,11 @@ const skip_song = (message, server_queue) => {
 
 const disconnect = (message, server_queue) => {
     if (!message.member.voice.channel) return message.channel.send('You need to be in a channel to execute this command!');
-    message.channel.send('Peace out :v:')
-    server_queue.songs = [];
-    server_queue.connection.dispatcher.end();
-}
-/*
-module.exports = {
-    name: 'play',
-    aliases: ['p'],
-    description: "Joins and plays a video from youtube.",
-    async execute(client, message, args){
-        const voiceChannel = message.member.voice.channel;
-
-        if (!voiceChannel) return message.channel.send('You need to be in a voice channel, dummy!');
-        if (!args.length) return message.channel.send('You forgot to add a link/word idiot.');
-
-        const validURL = (str) =>{
-            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([w#!:.?+=&%!\-\/]))?/;
-            if (!regex.test(str)){
-                return false;
-            } else{
-                return true;
-            }
-        }
-
-        if (validURL(args[0])){
-            const connection = await voiceChannel.join();
-            const stream = ytdl(args[0], {filter: 'audioonly'});
-
-            connection.play(stream, {seek: 0, volume: 1})
-            .on('finish', () => {
-                voiceChannel.leave();
-            });
-
-            await message.reply(`:thumbsup: Now Playing ***${video.title}***`)
-
-            return
-        }
-
-        const connection = await voiceChannel.join();
-        const videoFinder = async (query) => {
-            const videoResult = await ytSearch(query); //gets a list of the search results
-
-            return (videoResult.videos.length > 1) ?  videoResult.videos[0] : null; //chooses first video
-        }
-
-        const video = await videoFinder(args.join(' '));
-
-        if (video){
-            const stream = ytdl(video.url, {filter: 'audioonly'});
-            connection.play(stream, {seek: 0, volume: 1})
-            .on('finish', () => {
-                voiceChannel.leave();
-            });
-            await message.reply(`:thumbsup: Now Playing ***${video.title}***`)
-        } else{
-            message.channel.send('Video not found')
-        }
+    try{ 
+        message.client.queue.delete(message.guild.id);
+        server_queue.songs = [];
+        server_queue.connection.dispatcher.end();
+    } catch (error){
+        console.log(error);
     }
-}*/
+}
